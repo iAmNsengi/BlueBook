e40926576005e803611626787cf22f6786e50980
iAmNsengi <iamnsengi@icloud.com>
2022-05-06 12:02:07 +0300

    ft: isLoggedIn error handling

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -6,8 +6,26 @@ export const isLoggedIn = async (req, res, next) => {
     if (!token)
       return res
         .status(401)
-              .json({ message: "Unauthorized -- No token was provided" });
-    
+        .json({ message: "Unauthorized -- No token was provided" });
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded)
+      return res.status(401).json({ message: "Unauthorized -- Invalid token" });
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: "Unauthorized -- User not found" });
+
+    req.user = user;
     next();
-  } catch (error) {}
+  } catch (error) {
+    console.log(
+      "An internal server error occurred in isLoggedIn middleware",
+      error.message
+    );
+    return res
+      .status(500)
+      .json({ message: `An internal server error occurred, ${error.message}` });
+  }
 };
