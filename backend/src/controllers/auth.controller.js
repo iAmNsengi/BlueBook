59d892310e758986579dbdae55342641e4d90c1d
iAmNsengi <iamnsengi@icloud.com>
2022-05-07 13:31:20 +0300

    ft: update profile method

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import RegexCraft from "regexcraft";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -98,5 +99,19 @@ export const logout = (req, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-  res.send("Update profile");
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic)
+      return res.status(400).json({ message: "Profile picture is required!" });
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+    return res.status(200).json(updatedUser);
+  } catch (error) {}
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -12,6 +12,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/update-profile", isLoggedIn, updateProfile);
+router.put("/update-profile", isLoggedIn, updateProfile);
 
 export default router;
