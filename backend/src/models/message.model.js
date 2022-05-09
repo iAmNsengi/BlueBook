c2a72f901e6bed029348b25af6db0be206871db8
iAmNsengi <iamnsengi@icloud.com>
2022-05-09 15:15:02 +0300

    add: Message model

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    receiverId: {
+      ref: "User",
+      required: true,
+    },
+    text: { type: String },
+    image: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
