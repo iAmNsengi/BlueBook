17251b0b9ebb0b36758facbf3ebd9e82c00867d1
iAmNsengi <iamnsengi@icloud.com>
2022-05-10 15:38:07 +0300

    ft: get messages for chat

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,15 +1,35 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
-    console.log(loggedInUserId);
 
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
 
     return res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("An internal server error occured", error.message);
+    return res
+      .status(500)
+      .json({ message: `An internal server error occurred, ${error.message}` });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: senderId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: senderId },
+      ],
+    });
+    return res.status(200).json(messages);
   } catch (error) {
     console.log("An internal server error occured", error.message);
     return res
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", isLoggedIn, getUsersForSidebar);
+router.get("/:id", isLoggedIn, getMessages);
 
 export default router;
