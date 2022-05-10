e332a5708dda7e88ba77223d45a2e3766ff2ccdb
iAmNsengi <iamnsengi@icloud.com>
2022-05-10 15:25:15 +0300

    ft: gerUsers for sidebar

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,15 @@
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    return res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("An internal server error occured", error.message);
+    return res
+      .status(500)
+      .json({ message: `An internal server error occurred, ${error.message}` });
+  }
+};
