5a3250ecff60494c8d39d971cabb1c8f9b9d8599
iAmNsengi <iamnsengi@icloud.com>
2022-05-15 15:50:32 +0300

    ft: send message

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -35,5 +36,37 @@ export const getMessages = async (req, res) => {
     return res
       .status(500)
       .json({ message: `An internal server error occurred, ${error.message}` });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+    // todo: realtime functionality to go here
+
+    return res.status(200).json(newMessage);
+  } catch (error) {
+    console.log("An internal server error occured", error.message);
+    return res.status(500).json({
+      message: `An internal server error occurred, ${error.message}`,
+    });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
-import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", isLoggedIn, getUsersForSidebar);
 router.get("/:id", isLoggedIn, getMessages);
+
+router.get("/send/:id", isLoggedIn, sendMessage);
 
 export default router;
