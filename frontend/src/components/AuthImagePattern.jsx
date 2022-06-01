f8e2a40eff9964ae933680df89cf6bafc7e5870d
iAmNsengi <iamnsengi@icloud.com>
2022-06-01 12:52:47 +0300

    fix: UI bg

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthImagePattern = ({ title, subtitle }) => {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base">
+    <div className="hidden lg:flex items-center justify-center bg-primary/10 p-12">
       <div className="max-w-md text-center">
         <div className="grid grid-cols-3 gap-3 mb-8">
           {[...Array(9)].map((_, i) => (
