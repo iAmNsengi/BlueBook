7faaa8b4794180889a452313d3b5b770c134e0d3
iAmNsengi <iamnsengi@icloud.com>
2022-06-09 10:10:49 +0300

    ft: initial navbar UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { Link } from "react-router-dom";
+import { MessagesSquare } from "lucide-react";
 
 const Navbar = () => {
-  return <div>Navbar component</div>;
+  const { logout, authUser } = useAuthStore();
+
+  return (
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
+              <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessagesSquare className="w-5 h-5 text-primary" />
+              </div>
+              <h1>Vuga</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
