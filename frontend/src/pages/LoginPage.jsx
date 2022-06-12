646eeefec5d3a25fe005f66b8bfdb8cd5386ddb0
iAmNsengi <iamnsengi@icloud.com>
2022-06-12 10:43:14 +0300

    add: login method in store

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState();
+  const { login, isLoggingIn } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login(formData);
+  };
+
   return <div>Login Page</div>;
 };
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -37,10 +37,23 @@ export const useAuthStore = create((set) => ({
     try {
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
-      toast.success("Logged out successfully!");
+      toast.success("Logged out successfully");
     } catch (error) {
       console.log("Error in logout", error);
-      error.toast(error.response.data.message);
+      toast.error(error.response.data.message);
+    }
+  },
+  login: async (data) => {
+    if (data) set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Welcome back 😊");
+    } catch (error) {
+      console.log("Error in login", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
     }
   },
 }));
