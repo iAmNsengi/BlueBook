fefbc6a7e48a5017847210ec191dc1622e4d651a
iAmNsengi <iamnsengi@icloud.com>
2022-06-27 13:48:07 +0300

    fix: render themes options

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useThemeStore } from "../store/useThemeStore";
+import { THEMES } from "../constants";
 
 const SettingsPage = () => {
   const PREVIEW_MESSAGES = [
@@ -19,6 +20,30 @@ const SettingsPage = () => {
           <p className="text-sm text-base-content/70">
             Choose a theme of your preference
           </p>
+        </div>
+        <div className="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 gap-2">
+          {THEMES.map((t) => (
+            <button
+              key={t}
+              className={`group flex flex-col items-center gap-1.5 p-2 rounded-lg transition-colors ${
+                theme === t ? "bg-base-200" : "hover:bg-base-200/50"
+              }`}
+              onClick={() => setTheme(t)}
+            >
+              <div
+                className="relative h-8 w-full rounded-md overflow-hidden"
+                data-theme={t}
+              >
+                <div className="absolute inset-0 grid grid-cols-4 gap-px p-1">
+                  <div className="rounded bg-primary"></div>
+                  <div className="rounded bg-secondary"></div>
+                  <div className="rounded bg-accent"></div>
+                  <div className="rounded bg-neutral"></div>
+                </div>
+              </div>
+              <span>{t.charAt(0).toUpperCase() + t.slice(1)} </span>
+            </button>
+          ))}
         </div>
       </div>
     </div>
