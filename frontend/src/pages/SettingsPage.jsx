7c108b772e6049f989932038de5d57b0229e2945
iAmNsengi <iamnsengi@icloud.com>
2022-06-27 13:35:57 +0300

    fix: remove color white for themes to work

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import { useAuthStore } from "../store/useAuthStore";
+import { useThemeStore } from "../store/useThemeStore";
 
 const SettingsPage = () => {
-  const handleImageUpload = async () => {};
-  return <div>Settings page</div>;
+  const PREVIEW_MESSAGES = [
+    { id: 1, content: "Hey! How's it going mhen!?", isSent: false },
+    {
+      id: 2,
+      content: "Doing great man, Just working on some features",
+      isSent: true,
+    },
+  ];
+  const { theme, setTheme } = useThemeStore();
+  return (
+    <div className="h-screen container mx-auto px-4 pt-20 max-w-5xl">
+      <div className="space-y-6">
+        <div className="flex flex-col gap-1">
+          <h2 className="text-lg font-semibold">Theme</h2>
+          <p className="text-sm text-base-content/70">
+            Choose a theme of your preference
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
