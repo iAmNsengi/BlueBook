15de9227fc53959fb3e416c782d689a9baa4207a
iAmNsengi <iamnsengi@icloud.com>
2022-07-02 16:07:53 +0300

    ft: add contacts sidebar header

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
+import SidebarSkeleton from "./skeletons/SidebarSkeleton";
+import { Users } from "lucide-react";
 
 const Sidebar = () => {
   const { users, getUsers, selectedUser, setSelectedUser, isUsersLoading } =
@@ -11,7 +13,14 @@ const Sidebar = () => {
 
   if (isUsersLoading) return <SidebarSkeleton />;
 
-  return <div>Sidebar</div>;
+  return (
+    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+      <div className="border-b border-base-300 w-full p-5">
+        <Users className="size-6" />
+        <span className="font-medium hidden lg:block">Contacts</span>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
