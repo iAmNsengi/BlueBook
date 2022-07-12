00ccc4c75f1973ccaf4fdca80091787f55d87133
iAmNsengi <iamnsengi@icloud.com>
2022-07-12 14:06:37 +0300

    ft: add send message method

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -32,6 +32,19 @@ export const useChatStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isMessageLoading: false });
+    }
+  },
+  sendMessage: async (messageData) => {
+    try {
+      const { selectedUser, messages } = get();
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      console.error("Error in sendMessage", error);
+      toast.error(error.response.data.message);
     }
   },
 
