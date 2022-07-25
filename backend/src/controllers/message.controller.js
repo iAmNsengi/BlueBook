337c21e9686ea3eebedff7864216accd9b21bd8f
iAmNsengi <iamnsengi@icloud.com>
2022-07-25 19:16:49 +0300

    chore: socket send message in real time

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -66,7 +67,11 @@ export const sendMessage = async (req, res) => {
     });
 
     await newMessage.save();
-    // todo: realtime functionality to go here
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     return res.status(201).json(newMessage);
   } catch (error) {
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,11 +11,25 @@ const io = new Server(server, {
   },
 });
 
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+// store all online users
+const userSocketMap = {};
+
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
+  const userId = socket.handshake.query.userId;
+  userSocketMap[userId] = socket.id;
+
+  // used to broadcast the message to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
