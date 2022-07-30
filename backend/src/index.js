d4aac6ce1dd9d04fe7da661dc12e782b82819ddb
iAmNsengi <iamnsengi@icloud.com>
2022-07-30 20:31:54 +0300

    fix: production env variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
