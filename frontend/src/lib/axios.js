5efc17e12464bea6cc597c154e0a4f8386129248
iAmNsengi <iamnsengi@icloud.com>
2022-08-02 17:18:07 +0300

    ft: add hosted backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "/api",
+      : "https://vuga-backend.onrender.com/api",
   withCredentials: true,
 });
