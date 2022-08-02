96970c43d44f8c48fdbfa558251ecca3b714d285
iAmNsengi <iamnsengi@icloud.com>
2022-08-02 17:10:36 +0300

    ft: add hosted frontend url

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, io, server } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 import path from "path";
 
 dotenv.config();
@@ -15,18 +15,23 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://vuga.onrender.com/"],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
