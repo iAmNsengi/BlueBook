fec63bcf3b85612b57a56a6f29f5a3be751d136d
iAmNsengi <iamnsengi@icloud.com>
2022-08-06 18:16:59 +0300

    fix: socket configs

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -88,9 +88,27 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL, { query: { userId: authUser._id } });
+    const socket = io(BASE_URL, {
+      query: { userId: authUser._id },
+      withCredentials: true,
+      transports: ["websocket", "polling"],
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+      reconnectionAttempts: 5,
+    });
+
     socket.connect();
     set({ socket });
+
+    socket.on("connect", () => {
+      console.log("Socket connected successfully");
+    });
+
+    socket.on("connect_error", (error) => {
+      console.error("Socket connection error:", error);
+    });
+
     socket.on("getOnlineUsers", (userIds) => {
       set({ onlineUsers: userIds });
     });
