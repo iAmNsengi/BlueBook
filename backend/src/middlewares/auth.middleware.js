5d9bb6d458f7798203713210887ad372c291528f
iAmNsengi <iamnsengi@icloud.com>
2022-08-08 19:05:40 +0300

    fix: check logged in middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -4,20 +4,62 @@ import User from "../models/user.model.js";
 export const isLoggedIn = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if (!token)
-      return res
-        .status(401)
-        .json({ message: "Unauthorized -- No token was provided" });
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (!decoded)
-      return res.status(401).json({ message: "Unauthorized -- Invalid token" });
 
-    const user = await User.findById(decoded.userId).select("-password");
-    if (!user)
-      return res
-        .status(404)
-        .json({ message: "Unauthorized -- User not found" });
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized -- No token was provided",
+      });
+    }
 
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (jwtError) {
+      // Clear the invalid cookie
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
+        domain:
+          process.env.NODE_ENV === "development"
+            ? "localhost"
+            : ".onrender.com",
+        path: "/",
+      });
+
+      if (jwtError.name === "TokenExpiredError") {
+        return res.status(401).json({
+          message: "Token has expired - Please login again",
+        });
+      }
+
+      return res.status(401).json({
+        message: "Unauthorized -- Invalid token",
+      });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password").lean(); // Use lean() for better performance
+
+    if (!user) {
+      // Clear cookie if user not found
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
+        domain:
+          process.env.NODE_ENV === "development"
+            ? "localhost"
+            : ".onrender.com",
+        path: "/",
+      });
+
+      return res.status(404).json({
+        message: "Unauthorized -- User not found",
+      });
+    }
+
+    // Add token expiration time to request for potential refresh logic
+    req.tokenExp = decoded.exp;
     req.user = user;
     next();
   } catch (error) {
@@ -25,8 +67,19 @@ export const isLoggedIn = async (req, res, next) => {
       "An internal server error occurred in isLoggedIn middleware",
       error.message
     );
+
+    // Clear cookie on server error to be safe
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
+      domain:
+        process.env.NODE_ENV === "development" ? "localhost" : ".onrender.com",
+      path: "/",
+    });
+
     return res.status(500).json({
-      message: `An internal server error occurred in isLogin, ${error.message}`,
+      message: "An internal server error occurred - Please try again",
     });
   }
 };
