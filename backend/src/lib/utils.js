111f36153f5c84e2b3153c1a36b5a0a2c7e3807d
iAmNsengi <iamnsengi@icloud.com>
2022-08-08 18:58:29 +0300

    fix: generating jwt

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,11 +4,18 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 1000,
+
+  // Cookie configuration based on environment
+  const cookieOptions = {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     httpOnly: true, // prevent XSS attacks
-    sameSite: process.env.NODE_ENV == "development" ? "strict" : "None",
+    sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
     secure: process.env.NODE_ENV !== "development",
-  });
+    domain:
+      process.env.NODE_ENV === "development" ? "localhost" : ".onrender.com", // Allow sharing across subdomains
+    path: "/", // Ensuring cookie is available across all paths
+  };
+
+   res.cookie("jwt", token, cookieOptions);
   return token;
 };
