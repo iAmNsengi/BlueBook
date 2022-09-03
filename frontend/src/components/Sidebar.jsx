f869ec3ac49fae685e53442cee2703b42325bd6e
iAmNsengi <iamnsengi@icloud.com>
2022-09-03 16:45:42 +0300

    fix: prod error optional chaining

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -27,8 +27,6 @@ const Sidebar = () => {
 
   const searchForUsers = (e) => {
     e.preventDefault();
-    console.log(searchedUser, "searcheduser=-------");
-
     searchUsers(searchedUser);
   };
 
@@ -82,8 +80,8 @@ const Sidebar = () => {
           >
             <div className="relative mx-auto lg:mx-0">
               <img
-                src={user.profilePic || "avatar.png"}
-                alt={user.fullName}
+                src={user?.profilePic || "avatar.png"}
+                alt={user?.fullName}
                 className="size-12 object-cover rounded-full"
               />
               {onlineUsers.includes(user?._id) && (
