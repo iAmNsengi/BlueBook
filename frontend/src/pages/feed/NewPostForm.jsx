21d35c9ae100dc95216dd030906da53d3fe67203
iAmNsengi <iamnsengi@icloud.com>
2022-10-04 12:45:34 +0300

    ft: move to tiny mce editor instead of froala editor

--- frontend/src/pages/feed/NewPostForm.jsx
+++ frontend/src/pages/feed/NewPostForm.jsx
@@ -1,13 +1,13 @@
 import { Image, Send, X } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
-import FroalaEditorComponent from "react-froala-wysiwyg";
+import { useRef, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 import toast from "react-hot-toast";
 
 const NewPostForm = () => {
   const [editorContent, setEditorContent] = useState("");
   const [image, setImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
-  const [isEditorReady, setIsEditorReady] = useState(false);
+  const editorRef = useRef(null);
 
   const handleImageChange = (e) => {
     e.preventDefault();
@@ -37,7 +37,7 @@ const NewPostForm = () => {
 
   const fileInputRef = useRef(null);
 
-  const handleModelChange = (content) => {
+  const handleEditorChange = (content, editor) => {
     setEditorContent(content);
   };
 
@@ -52,39 +52,45 @@ const NewPostForm = () => {
     // Add your submit logic here
   };
 
-  useEffect(() => {
-    // Small delay to ensure DOM is ready
-    const timer = setTimeout(() => {
-      setIsEditorReady(true);
-    }, 100);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <form onSubmit={handleSubmit} className="px-4 py-6 rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-4">Add Post</h2>
-      {isEditorReady && (
-        <FroalaEditorComponent
-          model={editorContent}
-          onModelChange={handleModelChange}
-          config={{
-            toolbarButtons: [
-              "bold",
-              "italic",
-              "underline",
-              "paragraphFormat",
-              "align",
-              "formatOL",
-              "formatUL",
-              "insertHR",
-              "clearFormatting",
-              "insertImage",
-            ],
-            imageUpload: true,
-          }}
-        />
-      )}
+      <Editor
+        apiKey="iimwa8mt2kajduemd6v24b2kurxwpkyop5m0tjigba1xg4eh"
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        value={editorContent}
+        onEditorChange={handleEditorChange}
+        init={{
+          height: 200,
+          menubar: false,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "preview",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | " +
+            "bold italic underline | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
       <div className="mt-4 flex items-center justify-end gap-2">
         {imagePreview && (
           <div className="mb-3 flex items-center gap-2">
