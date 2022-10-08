00873d34aa67bc67de46e4a9a5f608b652d39321
iAmNsengi <iamnsengi@icloud.com>
2022-10-08 16:19:25 +0300

    ft: Posts UI render

--- frontend/src/pages/feed/Feed.jsx
+++ frontend/src/pages/feed/Feed.jsx
@@ -1,48 +1,163 @@
-import { MessageCircle, ThumbsUp } from "lucide-react";
+import { Bookmark, Heart, Loader2, MessageCircle, Share2 } from "lucide-react";
 import { formatMessageTime } from "../../lib/utils";
 import { usePostStore } from "../../store/usePostStore";
+import { useEffect, useState } from "react";
 
 const Feed = () => {
-  const { posts } = usePostStore();
+  const { posts, getAllPosts, isLoadingPosts } = usePostStore();
+  const [likedPosts, setLikedPosts] = useState(new Set());
+  const [savedPosts, setSavedPosts] = useState(new Set());
+
+  useEffect(() => {
+    getAllPosts();
+  }, [getAllPosts]);
+
+  const handleLike = (postId) => {
+    setLikedPosts((prev) => {
+      const newLiked = new Set(prev);
+      if (newLiked.has(postId)) {
+        newLiked.delete(postId);
+      } else {
+        newLiked.add(postId);
+      }
+      return newLiked;
+    });
+  };
+
+  const handleSave = (postId) => {
+    setSavedPosts((prev) => {
+      const newSaved = new Set(prev);
+      if (newSaved.has(postId)) {
+        newSaved.delete(postId);
+      } else {
+        newSaved.add(postId);
+      }
+      return newSaved;
+    });
+  };
+
+  if (isLoadingPosts) {
+    return (
+      <div className="flex justify-center items-center min-h-[200px]">
+        <Loader2 className="animate-spin h-8 w-8 text-primary" />
+      </div>
+    );
+  }
+
   return (
-    <div className="p-4">
-      {posts.map((post) => {
-        <div className="grid grid-cols-1 gap-4">
-          <div className="card bg-base-100 shadow-lg transition-transform transform hover:scale-105">
-            <div className="card-header flex items-center p-4 border-b">
-              <img
-                src={post?.author?.profilePic || "avatar.png"}
-                alt={post?.author?.fullName}
-                className="rounded-full mr-2"
-              />
-              <div>
-                <h2 className="font-bold">{post?.author?.fullName} </h2>
-                <p className="text-sm text-gray-500">
-                  {formatMessageTime(post?.createdAt)}{" "}
-                </p>
+    <div className="max-w-5xl mx-auto px-4 py-6 space-y-6">
+      {posts.length === 0 ? (
+        <div className="text-center py-10">
+          <p className="text-lg font-medium">No posts yet</p>
+          <p className="text-sm opacity-70">Be the first to share something!</p>
+        </div>
+      ) : (
+        posts.map((post) => (
+          <div
+            key={post.id}
+            className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300"
+          >
+            {/* Header */}
+            <div className="flex items-center justify-between p-4 border-b">
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="w-10 h-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img
+                      src={post?.author?.profilePic || "/avatar.png"}
+                      alt={post?.author?.fullName}
+                      className="object-cover"
+                      loading="lazy"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <h3 className="font-semibold text-sm">
+                    {post?.author?.fullName}
+                  </h3>
+                  <p className="text-xs opacity-70">
+                    {formatMessageTime(post?.createdAt)}
+                  </p>
+                </div>
               </div>
+              <button className="btn btn-ghost btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                  />
+                </svg>
+              </button>
             </div>
+
+            {/* Image */}
             {post?.image && (
-              <img
-                src={post?.image}
-                alt="Post Image"
-                className="w-full rounded-t-lg"
-              />
+              <figure className="relative">
+                <img
+                  src={post.image}
+                  alt="Post"
+                  className="w-full object-cover max-h-[32rem]"
+                  loading="lazy"
+                />
+              </figure>
             )}
-            <div className="card-body p-4">
-              {post?.content && <div>{post?.content} </div>}
-              <div className="card-actions justify-end mt-4">
-                <button className="btn btn-primary">
-                  <ThumbsUp />{" "}
-                </button>
-                <button className="btn btn-secondary">
-                  <MessageCircle />{" "}
+
+            {/* Content */}
+            {post?.content && (
+              <div className="p-4 py-10">
+                <div className="prose prose-sm max-w-none">
+                  <div dangerouslySetInnerHTML={{ __html: post.content }} />
+                </div>
+              </div>
+            )}
+
+            {/* Action Buttons */}
+            <div className="p-3 border-t">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                  <button
+                    className={`btn btn-ghost btn-circle ${
+                      likedPosts.has(post.id) ? "text-red-500" : ""
+                    }`}
+                    onClick={() => handleLike(post.id)}
+                  >
+                    <Heart
+                      className={`h-6 w-6 transition-colors duration-200 ${
+                        likedPosts.has(post.id) ? "fill-current" : ""
+                      }`}
+                    />
+                  </button>
+                  <button className="btn btn-ghost btn-circle">
+                    <MessageCircle className="h-6 w-6" />
+                  </button>
+                  <button className="btn btn-ghost btn-circle">
+                    <Share2 className="h-6 w-6" />
+                  </button>
+                </div>
+                <button
+                  className={`btn btn-ghost btn-circle ${
+                    savedPosts.has(post.id) ? "text-yellow-500" : ""
+                  }`}
+                  onClick={() => handleSave(post.id)}
+                >
+                  <Bookmark
+                    className={`h-6 w-6 transition-colors duration-200 ${
+                      savedPosts.has(post.id) ? "fill-current" : ""
+                    }`}
+                  />
                 </button>
               </div>
             </div>
           </div>
-        </div>;
-      })}
+        ))
+      )}
     </div>
   );
 };
