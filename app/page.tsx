248c1498dac394f5bd2470d75eb29e2e42411631
Eliezer Nsengi <iamnsengi@icloud.com>
2023-08-02 12:30:35 +0200

    add: clerk config middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>Let's build this</p>;
+  return (
+    <>
+      <p>Let's build this</p>
+      <Button>Click Me</Button>
+    </>
+  );
 }
