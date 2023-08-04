3c6c52987f0ec23cef11997a7627d5defa7e42e9
Eliezer Nsengi <iamnsengi@icloud.com>
2023-08-04 12:47:24 +0200

    update: header UI

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,10 +11,11 @@ import {
 const Header = () => {
   const { user } = useUser();
   return (
-    <div>
+    <div className="flex items-center justify-between p-5">
       {user && (
-        <h1>
-          {user?.firstName} {`'s`} Space
+        <h1 className="text-2xl font-bold">
+          {user?.firstName}
+          {`'s`} Space
         </h1>
       )}
 
