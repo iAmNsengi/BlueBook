bb4cafc86399b9f819e83a58b68ae2b595a691b6
Eliezer Nsengi <iamnsengi@icloud.com>
2023-08-05 13:22:39 +0200

    add: sidebar UI and responsive drawer

---
+++ components/NewDocumentButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const NewDocumentButton = () => {
+  return (
+    <div>
+      <Button>New Document</Button>
+    </div>
+  );
+};
+
+export default NewDocumentButton;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import NewDocumentButton from "./NewDocumentButton";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <div className="hidden md:inline">
+        <NewDocumentButton />
+      </div>
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon />
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{/** options */}</div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
