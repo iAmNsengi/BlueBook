869b7d6a9b045f1708a83bb674790801fbb547d1
Eliezer Nsengi <nsengitech@gmail.com>
2024-12-11 22:18:26 +0200

    rename file

--- freecodecamp_cash_register_project.js
+++ freecodecamp_cash_register_project.js
@@ -1,27 +1,25 @@
 const currencyUnit = {
-  "PENNY": 1,
-  "NICKEL": 5, 
-  "DIME": 10,
-  "QUARTER": 25,
-  "ONE": 100,
-  "FIVE": 500,
-  "TEN": 1000,
-  "TWENTY": 2000,
-  "ONE HUNDRED": 10000
-}
-
+  PENNY: 1,
+  NICKEL: 5,
+  DIME: 10,
+  QUARTER: 25,
+  ONE: 100,
+  FIVE: 500,
+  TEN: 1000,
+  TWENTY: 2000,
+  "ONE HUNDRED": 10000,
+};
 
 function checkCashRegister(price, cash, cid) {
-
   let changeSum = cash * 100 - price * 100;
   let changeSumCheck = changeSum;
   let change = [];
-  let status = '';
+  let status = "";
 
   let cidSum = 0;
-  let filteredCid = cid.filter(elem => elem[1] !== 0).reverse();
+  let filteredCid = cid.filter((elem) => elem[1] !== 0).reverse();
 
-  filteredCid.forEach(elem => {
+  filteredCid.forEach((elem) => {
     let curr = elem[0];
     let currSum = elem[1] * 100;
     cidSum += currSum;
@@ -37,15 +35,25 @@ function checkCashRegister(price, cash, cid) {
   });
 
   if (changeSum > 0) {
-    status = 'INSUFFICIENT_FUNDS';
+    status = "INSUFFICIENT_FUNDS";
     change = [];
   } else if (changeSum == 0 && changeSumCheck == cidSum) {
-    status = 'CLOSED';
+    status = "CLOSED";
     change = cid;
   } else {
-    status = 'OPEN';
+    status = "OPEN";
   }
-  return { 'status': status, 'change': change };
+  return { status: status, change: change };
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
