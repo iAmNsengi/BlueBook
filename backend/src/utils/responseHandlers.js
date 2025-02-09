964440af08d69c989e458c9b143a451c898d3aba
iAmNsengi <iamnsengi@icloud.com>
2025-02-09 19:37:20 +0200

    ft: response handlers

---
+++ backend/src/utils/responseHandlers.js
@@ -0,0 +1,8 @@
+export const successResponse = (res, statusCode, data = null) =>
+  res.status(statusCode).json({ success: true, data });
+
+export const errorResponse = (err, req, res, next) =>
+  res.status(err?.cause?.code === 11000 ? 409 : err?.statusCode || 500).json({
+    success: false,
+    message: err?.message || "An internal server error occurred",
+  });
