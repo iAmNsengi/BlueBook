4621c3ce9c9c0a08e3ab75a94e10cb1324c28019
iAmNsengi <iamnsengi@icloud.com>
2025-02-09 18:55:02 +0200

    ft: add requests limiters to routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import compression from "compression";
 
+import appRoutes from "./routes/index.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 import { corsOptions } from "./lib/corsOptions.js";
-import appRoutes from "./routes/index.js";
+import { requestsLimit } from "./utils/rateLimit.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(compression());
 
-// CORS configuration
 app.use(corsOptions);
-
-// Add security headers
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
+app.use(requestsLimit);
 app.use(appRoutes);
 
 server.listen(PORT, () => {
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import {
   checkAuth,
   deleteAccount,
@@ -9,11 +10,19 @@ import {
   updateProfile,
 } from "../controllers/auth.controller.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import {
+  loginRequestsLimiter,
+  signupRequestsLimiter,
+} from "../utils/rateLimit.js";
 
 const router = express.Router();
 
+router.use("/signup", signupRequestsLimiter);
 router.post("/signup", signup);
+
+router.use("/login", loginRequestsLimiter);
 router.post("/login", login);
+
 router.post("/logout", logout);
 router.put("/update-profile", isLoggedIn, updateProfile);
 router.delete("/delete-account", isLoggedIn, deleteAccount);
