8ab5dc4c66814b7799b7e237772d91d3edbd7fdb
iAmNsengi <iamnsengi@icloud.com>
2025-02-10 17:00:02 +0200

    fx: 😒 redis configs for prod

--- backend/src/utils/redis/client.js
+++ backend/src/utils/redis/client.js
@@ -1,9 +1,9 @@
 import redis from "redis";
 
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  //   password: process.env.REDIS_PASSWORD || undefined,
 });
 
 redisClient.on("error", (error) => console.log("Redis error: ", error));
